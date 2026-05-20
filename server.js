85edd6c9b4d9ae4eac5d52dc0cf198c1c4a7103d
gmmouraFag <161531153+gmmouraFag@users.noreply.github.com>
2026-05-20 20:35:10 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
 const express = require("express");
-
 require("dotenv").config();
-
+const { Pool } = require("pg");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 const message =
-    process.env.MESSAGE ||
-    "Deploy funcionando!";
-
+process.env.MESSAGE ||
+"Deploy funcionando!";
+// conexão PostgreSQL
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: {
+rejectUnauthorized: false
+}
+});
+// rota principal
 app.get("/", (req, res) => {
-    res.send(`
-        <h1>Servidor Express no Render</h1>
-        <p>${message}</p>
-    `);
+res.send(`
+<h1>Servidor Express no Render</h1>
+<p>${message}</p>
+`);
 });
-
+// rota api
 app.get("/api", (req, res) => {
-    res.json({
-        mensagem: "API funcionando",
-        status: "online"
-    });
-});
+res.json({
+mensagem: "API funcionando",
 
+status: "online"
+});
+});
+// rota banco de dados
+app.get("/database", async (req, res) => {
+try {
+const result = await pool.query("SELECT NOW()");
+res.json({
+status: "Banco conectado",
+horario: result.rows[0]
+});
+} catch (error) {
+res.status(500).json({
+erro: error.message
+});
+}
+});
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+console.log(`Servidor rodando na porta ${PORT}`);
 });
